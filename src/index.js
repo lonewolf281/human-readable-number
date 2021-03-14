ae225e8039d11bc3454c21e8e2762f5d52395000
lonewolf281 <lonewolf0508@gmail.com>
2021-03-14 17:07:29 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    if (number == 0) return "zero";
+
+    const onesNames = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const tensNames = [
+        "",
+        "",
+        "twenty ",
+        "thirty ",
+        "forty ",
+        "fifty ",
+        "sixty ",
+        "seventy ",
+        "eighty ",
+        "ninety ",
+    ];
+
+    let hundreads = Math.floor(number / 100);
+    let tens = Math.floor((number - hundreads * 100) / 10);
+    let ones = number - hundreads * 100 - tens * 10;
+
+    if (tens == 1) {
+        tens = 0;
+        ones = number - hundreads * 100;
+    }
+    return hundreads
+        ? (
+              onesNames[hundreads] +
+              " hundred " +
+              tensNames[tens] +
+              onesNames[ones]
+          ).trim()
+        : (tensNames[tens] + onesNames[ones]).trim();
+};
